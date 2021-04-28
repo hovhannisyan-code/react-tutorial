9c363caa7ccc081172340a8e47055d74dc915c05
Gor Hovhannisyan <gor.cecunc@gmail.com>
2021-04-28 10:03:14 +0400

    remove search

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -52,22 +52,6 @@ const MenuNavbar = () => {
                     
                         {menuList}
                 </Navbar.Collapse>
-                <div>
-                    <form className="form-inline my-2 my-lg-0">
-                        <div className="input-group input-group-sm">
-                            <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Search..." />
-                            <div className="input-group-append">
-                                <button type="button" className="btn btn-secondary btn-number">
-                                    <FontAwesomeIcon icon={faSearch} className="mr-1" />
-                                </button>
-                            </div>
-                        </div>
-                        {/* <a className="btn btn-success btn-sm ml-3" href="cart.html">
-                            <i className="fa fa-shopping-cart"></i> Cart
-                            <span className="badge badge-light">3</span>
-                        </a> */}
-                    </form>
-                </div>
                 
             </Container>
 
